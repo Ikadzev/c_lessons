d6345ccc94feeb63d60738e4b171bc8f27794070
Valentin <ikadzev@mail.ru>
2023-10-17 22:41:30 +0700

    WIP 10.3

--- task10/task10.3.c
+++ task10/task10.3.c
@@ -21,34 +21,46 @@ char* trim(char* str, size_t len_s) {
 
 char* split(char* str, size_t len_s, size_t* size_array) {
     char* nw_str = trim(str, len_s);
+    printf("%s", nw_str);
+    printf("\n");
     size_t j = strlen(nw_str);
     char temp_str[j];
     size_t size_temp = 0;
     char arry[j][j];
     size_t size_arry = 0;
-    for (size_t i = 0; i < j; i++) {
-        if (str[i] != 32) {
-            temp_str[size_temp++] = str[i];
-        } else if (str[i] == 32) {
+    for (size_t i = 0; i < j + 1; i++) {
+        if ((nw_str[i] != 32) && (nw_str[i] != 0)) {
+            temp_str[size_temp++] = nw_str[i];
+        } else if (nw_str[i] == 32) {
             temp_str[size_temp] = 0;
-            for (size_t i = 0; i < size_temp; i++) {
-                temp_str[size_arry][i] = temp_str[i];
+            for (size_t q = 0; q < size_temp; q++) {
+                arry[size_arry][q] = temp_str[q];
             }
             size_temp = 0;
             size_arry++;
+        } else {
+            temp_str[size_temp] = 0;
+            for (size_t q = 0; q < size_temp; q++) {
+                arry[size_arry++][q] = temp_str[q];
+            }
+            size_temp = 0;
+            arry[size_arry][0] = 0;
         }
     }
-    size_array = size_arry; 
+    for (size_t i = 0; i < size_arry; i++) {
+        printf("%s ", arry[i]);
+    }
+    *size_array = size_arry; 
     return arry;
 }
 
 int main() {
     char* str = "   ab cd e      ";
     size_t len_s = strlen(str);
-    size_t* size_arry;
-    char* arry = split(str, len_s, size_arry);
-    for (size_t i = 0; i < *size_arry; i++) {
-        printf("%s\n", arry[i]);
+    size_t size_arry;
+    char* arry = split(str, len_s, &size_arry);
+    for (size_t i = 0; i < size_arry; i++) {
+        printf("%s ", arry[i]);
     }
     // char* new_str = trim(str, len_s);
     // printf("%s ", new_str);
