3aaee0294f84f523448088376b38ab79b08f5737
Valentin <ikadzev@mail.ru>
2023-10-21 22:08:33 +0700

    Updated 10.1

--- task10/task10.1.c
+++ task10/task10.1.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int palindrom(char* str, size_t len_s) {
+int palindrom(const char* str, size_t len_s) {
     char new_str[len_s];
     size_t j = 0;
     for (size_t i = 0; i < len_s; i++) {
         if (str[i] != ' ') {
-            new_str[j] = str[i];
-            printf("%c %c\n", str[i], new_str[j]);
-            j++;
+            new_str[j++] = str[i];
         }
     }
     new_str[j] = 0;
     int f = 1;
-    printf("%s\n", new_str);
     for (size_t q = 0; q < (j / 2)+1; q++) {
         if (new_str[q] != new_str[j - q - 1]) {
             f = 0;
@@ -23,8 +20,12 @@ int palindrom(char* str, size_t len_s) {
 }
 
 int main() {
-    char* str = "aba";
+    const char* str = " a   b a            ";
     size_t len_s = strlen(str);
     int f = palindrom(str, len_s);
-    printf("%d ", f);
+    if (f == 1) {
+        printf("Palindrom");
+    } else {
+        printf("Not palindrom");
+    }
 }
